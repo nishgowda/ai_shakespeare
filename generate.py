188a24cc6b0a5af7867fd7e18768bf49b47cce98
nish.gowda6@gmail.com <60044986+nishgowda@users.noreply.github.com>
2021-01-03 11:40:23 -0500

    add more epochs to training

---
+++ generate.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import torch
+from helpers import *
+from model import *
+import argparse
+
+def generate_text(decoder, prime_str='A', predict_len=100, temperature=0.8, cuda=False):
+    hidden = decoder.init_hidden(1)
+    prime_input = Variable(char_tensor(prime_str).unsqueeze(0))
+
+    if cuda:
+        hidden = hidden.cuda()
+        prime_input = prime_input.cuda()
+    predicted = prime_str
+
+    # Use priming string to "build up" hidden state
+    for p in range(len(prime_str) - 1):
+        _, hidden = decoder(prime_input[:,p], hidden)
+        
+    inp = prime_input[:,-1]
+    
+    for p in range(predict_len):
+        output, hidden = decoder(inp, hidden)
+        
+        # Sample from the network as a multinomial distribution
+        output_dist = output.data.view(-1).div(temperature).exp()
+        top_i = torch.multinomial(output_dist, 1)[0]
+
+        # Add predicted character to string and use as next input
+        predicted_char = all_characters[top_i]
+        predicted += predicted_char
+        inp = Variable(char_tensor(predicted_char).unsqueeze(0))
+        if cuda:
+            inp = inp.cuda()
+
+    return predicted
+
+# Write the shakespeare to a file
+def write_to_file(output):
+    options = ['ROMEO', 'JULIET','MACBETH', 'OTHELLO', 'KING_HENRY', 'CAPULET', 'MONTAGUE']
+    fi = f'output/{random.choice(options)}.txt'
+    print(fi)
+    with open(fi, "w") as f:
+        f.write(output)
+    print(f"Succesfully wrote shakespeare text to {fi}")
+
+# Run as standalone script
+if __name__ == '__main__':
+# Parse command line arguments
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('filename', type=str)
+    argparser.add_argument('-p', '--prime_str', type=str, default='A')
+    argparser.add_argument('-l', '--predict_len', type=int, default=100)
+    argparser.add_argument('-t', '--temperature', type=float, default=0.8)
+    argparser.add_argument('--cuda', action='store_true')
+    args = argparser.parse_args()
+    decoder = torch.load(args.filename)
+    del args.filename
+    write_to_file(generate_text(decoder, **vars(args)))
+
